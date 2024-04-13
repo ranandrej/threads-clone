cf91acc94e07957e129aa9becaa4df28c861c511
unknown <ranandrej@gmail.com>
2024-04-13 18:54:21 +0200

    Newest

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="bg-black fixed w-[calc(100%+1px)] h-full">
     <NuxtPage />
+    <NuxtLayout/>
     <CreatePost :class="[{'max-h-[100vh] transition-all duration-200 ease-in visible':userStore.isMenuOverlay},
   {'max-h-0 transition-all duration-200 ease-out invisible':!userStore.isMenuOverlay}]"/>
 
--- server/api/delete.js
+++ server/api/delete.js
@@ -1,32 +1,32 @@
-// import { serverSupabaseClient } from '#supabase/server'
+import { serverSupabaseClient } from '#supabase/server'
 
-// export default defineEventHandler(async (event) => {
-//     const supabase = await serverSupabaseClient(event)
+export default defineEventHandler(async (event) => {
+    const supabase = await serverSupabaseClient(event)
     
-//     try {
-//         const query = getQuery(event)
-//         const { error } = await supabase
-//             .from('Posts')
-//             .delete()
-//             .eq('id', query.id)
+    try {
+        const query = getQuery(event)
+        const { error } = await supabase
+            .from('Posts')
+            .delete()
+            .eq('id', query.id)
 
-//         if (error) {
-//             console.error('Error deleting post:', error.message)
-//             return {
-//                 statusCode: 500,
-//                 body: JSON.stringify({ error: 'Internal Server Error' })
-//             }
-//         }
+        if (error) {
+            console.error('Error deleting post:', error.message)
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ error: 'Internal Server Error' })
+            }
+        }
 
-//         return {
-//             statusCode: 200,
-//             body: JSON.stringify({ message: 'Post deleted successfully' })
-//         }
-//     } catch (error) {
-//         console.error('Error handling request:', error)
-//         return {
-//             statusCode: 500,
-//             body: JSON.stringify({ error: 'Internal Server Error' })
-//         }
-//     }
-// })
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Post deleted successfully' })
+        }
+    } catch (error) {
+        console.error('Error handling request:', error)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' })
+        }
+    }
+})
